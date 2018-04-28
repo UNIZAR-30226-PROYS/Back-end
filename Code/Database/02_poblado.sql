10e328107316c329dff77b90eaad1f0a0e50fb52
Miguel Escribano <miguescri@gmail.com>
2018-04-28 20:16:05 +0200

    Reordenación ficheros de autoamtización sql de postgres

---
+++ Code/Database/02_poblado.sql
@@ -0,0 +1,4 @@
+SELECT insert_new_user('A','alice@mail.net','Alice','ApassA');
+SELECT insert_new_user('B','bob@mail.net','Bob','BpassB');
+SELECT insert_new_user('C','charlie@mail.net','Charlie','CpassC');
+SELECT insert_new_user('D','dario@mail.net','Dario','DpassD');
