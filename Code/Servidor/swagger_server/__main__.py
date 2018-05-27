ee69b6c6692bcad2d173422a2fa3ea773a96c78e
Miguel Escribano <miguescri@gmail.com>
2018-05-28 01:24:42 +0200

    URL Web CORS mediante fichero  configuración
    Ahora para indicar si hace falta los headers de CORS o la URL de origen de la web
    Angular, no hace falta reconstruir con 'docker-compose build', sino modificar
    los parámetros en el fichero .env y usar 'docker-compose up -d' para actualizar la
    configuración.

--- Code/Servidor/swagger_server/__main__.py
+++ Code/Servidor/swagger_server/__main__.py
@@ -9,14 +9,14 @@ app.app.json_encoder = encoder.JSONEncoder
 app.app.config.from_object(BaseConfig)
 app.add_api('swagger.yaml', arguments={'title': 'Proyecto Cierzo API'})
 
-
-@app.app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:2595')
-    response.headers.add('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE, OPTIONS')
-    response.headers.add('Access-Control-Allow-Credentials', 'true')
-    return response
+if BaseConfig.WEB_CORS == 'true':
+    @app.app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Origin', BaseConfig.WEB_URI)
+        response.headers.add('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE, OPTIONS')
+        response.headers.add('Access-Control-Allow-Credentials', 'true')
+        return response
 
 
 def main():
